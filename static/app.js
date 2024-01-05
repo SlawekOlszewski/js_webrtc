e1af8d101a01f7d9494ffb87352a4c38296b0b19
slawomirolszewski <slawomir.olszew@gmail.com>
2024-01-05 13:50:32 +0100

    Handling disconnect

--- static/app.js
+++ static/app.js
@@ -207,6 +207,92 @@ function setupPeerConnection(stream) {
     }
   };
 
+  peerConnection.oniceconnectionstatechange = function () {
+    switch (peerConnection.iceConnectionState) {
+      case "closed": // This means connection is shut down and no longer handling requests.
+        // hangUpCall(); //Hangup instead of closevideo() because we want to record call end in db
+        console.log("hangUpCalls");
+        break;
+      case "failed":
+        checkStatePermanent("failed");
+        break;
+      case "disconnected":
+        checkStatePermanent("disconnected");
+        break;
+    }
+  };
+
+  async function isPermanentDisconnect() {
+    var isPermanentDisconnectFlag = false;
+    var videoIsAlive = false;
+    var audioIsAlive = false;
+
+    await peerConnection.getStats(null).then((stats) => {
+      stats.forEach((report) => {
+        if (
+          report.type === "inbound-rtp" &&
+          (report.kind === "audio" || report.kind === "video")
+        ) {
+          //check for inbound data only
+          if (report.kind === "audio") {
+            //Here we must compare previous data count with current
+            if (report.bytesReceived > audioReceivedByteCount) {
+              // If current count is greater than previous then that means data is flowing to other peer. So this disconnected or failed ICE state is temporary
+              audioIsAlive = true;
+            } else {
+              audioIsAlive = false;
+            }
+            audioReceivedByteCount = report.bytesReceived;
+          }
+          if (report.kind === "video") {
+            if (report.bytesReceived > videoReceivedBytetCount) {
+              // If current count is greater than previous then that means data is flowing to other peer. So this disconnected or failed ICE state is temporary
+              videoIsAlive = true;
+            } else {
+              videoIsAlive = false;
+            }
+            videoReceivedBytetCount = report.bytesReceived;
+          }
+          if (audioIsAlive || videoIsAlive) {
+            //either audio or video is being recieved.
+            isPermanentDisconnectFlag = false; //Disconnected is temp
+          } else {
+            isPermanentDisconnectFlag = true;
+          }
+        }
+      });
+    });
+
+    return isPermanentDisconnectFlag;
+  }
+
+  const customdelay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+  async function checkStatePermanent(iceState) {
+    videoReceivedBytetCount = 0;
+    audioReceivedByteCount = 0;
+
+    let firstFlag = await isPermanentDisconnect();
+
+    await customdelay(2000);
+
+    let secondFlag = await isPermanentDisconnect(); //Call this func again after 2 seconds to check whether data is still coming in.
+
+    if (secondFlag) {
+      //If permanent disconnect then we hangup i.e no audio/video is fllowing
+      if (iceState == "disconnected") {
+        // hangUpCall(); //Hangup instead of closevideo() because we want to record call end in db
+        console.log("hangUpCalls");
+      }
+    }
+    if (!secondFlag) {
+      //If temp failure then restart ice i.e audio/video is still flowing
+      if (iceState == "failed") {
+        peerConnection.restartIce();
+      }
+    }
+  }
+
   peerConnection
     .createOffer()
     .then((offer) => peerConnection.setLocalDescription(offer))
@@ -261,6 +347,10 @@ socket.on("connect", () => {
   startVideoStream(); // Restart video stream
   // Initialize or reinitialize WebRTC connection setup here
   setupPeerConnection(localStream);
+});
+
+socket.on("close", () => {
+  resetConnection();
 });
 
 function resetConnection() {
